a17690fe43d581ea13c067512545abf858922eb6
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-05-23 16:37:40 -0300

    feat: add cart and cart  item components

---
+++ app/_components/cart-item.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+import { CartContext, CartProduct } from "../_context/cart";
+import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
+import { Button } from "./ui/button";
+import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
+
+interface CartItemProps {
+  cartProduct: CartProduct;
+}
+
+const CartItem = ({ cartProduct }: CartItemProps) => {
+  const {
+    decreaseProductQuantity,
+    increaseProductQuantity,
+    removeProductFromCart,
+  } = useContext(CartContext);
+
+  const handleDecreaseQuantityClick = () =>
+    decreaseProductQuantity(cartProduct.id);
+
+  const handleIncreaseQuantityClick = () =>
+    increaseProductQuantity(cartProduct.id);
+
+  const handleRemoveClick = () => removeProductFromCart(cartProduct.id);
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        {/*Imagem e info */}
+        <div className="relative h-20 w-20 ">
+          <Image
+            src={cartProduct.imageUrl}
+            alt={cartProduct.name}
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+
+        <div className="space-y-1">
+          <h3 className="text-xs">{cartProduct.name}</h3>
+
+          <div className="flex items-center gap-1">
+            <h4 className="text-sm font-semibold">
+              {formatCurrency(
+                calculateProductTotalPrice(cartProduct) * cartProduct.quantity,
+              )}
+            </h4>
+
+            {cartProduct.discountPercentage > 0 && (
+              <span className="text-xs text-muted-foreground line-through">
+                {formatCurrency(
+                  Number(cartProduct.price) * cartProduct.quantity,
+                )}
+              </span>
+            )}
+          </div>
+
+          {/*Quanttidade*/}
+          <div className="flex items-center gap-3 text-center">
+            <Button
+              size="icon"
+              variant="ghost"
+              className="h-7 w-7 border border-solid border-muted-foreground"
+            >
+              <ChevronLeftIcon
+                size={16}
+                onClick={handleDecreaseQuantityClick}
+              />
+            </Button>
+
+            <span className="text-ms block w-4">{cartProduct.quantity}</span>
+            <Button size="icon" className="h-7 w-7">
+              <ChevronRightIcon
+                size={16}
+                onClick={handleIncreaseQuantityClick}
+              />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Botao de deletar */}
+      <Button
+        size="icon"
+        variant="ghost"
+        className="h-7 w-7 border border-solid border-muted-foreground"
+        onClick={handleRemoveClick}
+      >
+        <TrashIcon size={16} />
+      </Button>
+    </div>
+  );
+};
+
+export default CartItem;
