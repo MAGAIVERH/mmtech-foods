7d117cbe925914dbcde3ab17c9ee363332f64a17
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-05-29 09:34:47 -0300

    feat: creating the my-orders

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -1,73 +1,167 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../_context/cart";
 import CartItem from "./cart-item";
 import { Card, CardContent } from "./ui/card";
 import { formatCurrency } from "../_helpers/price";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { createOrder } from "../_actions/order";
+import { OrderStatus } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { Loader2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
 
 const Cart = () => {
-  const { products, totalDiscounts, subTotalPrice, totalPrice } =
+  const [isSubmitLoading, setIsSubmitLoading] = useState(false);
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
+
+  const { data } = useSession();
+
+  const { products, totalDiscounts, subTotalPrice, totalPrice, clearCart } =
     useContext(CartContext);
 
+  const handleFinishOrderClick = async () => {
+    if (!data?.user) return;
+
+    const restaurant = products[0].restaurant;
+
+    try {
+      setIsSubmitLoading(true);
+
+      await createOrder({
+        subTotalPrice,
+        totalDiscounts,
+        totalPrice,
+        deliveryFee: restaurant.deliveryFee,
+        deliveryTimeMinutes: restaurant.deliveryTimeMinutes,
+        restaurant: {
+          connect: { id: restaurant.id },
+        },
+        status: OrderStatus.CONFIRMED,
+        user: {
+          connect: { id: data.user.id },
+        },
+        products: {
+          createMany: {
+            data: products.map((product) => ({
+              productId: product.id,
+              quantity: product.quantity,
+            })),
+          },
+        },
+      });
+      clearCart();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitLoading(false);
+    }
+  };
+
   return (
-    <div className="flex h-full flex-col py-5 ">
-      {products.length > 0 ? (
-        <>
-          <div className="flex-auto space-y-4">
-            {products.map((product) => (
-              <CartItem key={product.id} cartProduct={product} />
-            ))}
-          </div>
+    <>
+      <div className="flex h-full flex-col py-5 ">
+        {products.length > 0 ? (
+          <>
+            <div className="flex-auto space-y-4">
+              {products.map((product) => (
+                <CartItem key={product.id} cartProduct={product} />
+              ))}
+            </div>
 
-          {/*Totais*/}
-          <div className="mt-6">
-            <Card>
-              <CardContent className="space-y-4 p-5">
-                <div className="flex items-center justify-between text-xs">
-                  <span className="text-xs text-muted-foreground">
-                    Subtotal
-                  </span>
-                  <span>{formatCurrency(subTotalPrice)}</span>
-                </div>
+            {/*Totais*/}
+            <div className="mt-6">
+              <Card>
+                <CardContent className="space-y-4 p-5">
+                  <div className="flex items-center justify-between text-xs">
+                    <span className="text-xs text-muted-foreground">
+                      Subtotal
+                    </span>
+                    <span>{formatCurrency(subTotalPrice)}</span>
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="flex items-center justify-between text-xs">
-                  <span className="text-xs text-muted-foreground">
-                    Descontos
-                  </span>
-                  <span>- {formatCurrency(totalDiscounts)}</span>
-                </div>
+                  <div className="flex items-center justify-between text-xs">
+                    <span className="text-xs text-muted-foreground">
+                      Descontos
+                    </span>
+                    <span>- {formatCurrency(totalDiscounts)}</span>
+                  </div>
 
-                <Separator className="h-[0.5px] text-muted-foreground" />
+                  <Separator className="h-[0.5px] text-muted-foreground" />
 
-                <div className="flex items-center justify-between text-xs">
-                  <span className="text-xs text-muted-foreground">Entrega</span>
+                  <div className="flex items-center justify-between text-xs">
+                    <span className="text-xs text-muted-foreground">
+                      Entrega
+                    </span>
 
-                  {Number(products?.[0].restaurant.deliveryFee) === 0 ? (
-                    <span className="uppercase text-primary">Grátis</span>
-                  ) : (
-                    formatCurrency(Number(products?.[0].restaurant.deliveryFee))
-                  )}
-                </div>
+                    {Number(products?.[0].restaurant.deliveryFee) === 0 ? (
+                      <span className="uppercase text-primary">Grátis</span>
+                    ) : (
+                      formatCurrency(
+                        Number(products?.[0].restaurant.deliveryFee),
+                      )
+                    )}
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="flex items-center justify-between text-xs font-semibold">
-                  <span>Total</span>
-                  <span>{formatCurrency(totalPrice)}</span>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-          {/*Finalizar Pedidos */}
-          <Button className="mt-6 w-full">Finalizar Pedido</Button>
-        </>
-      ) : (
-        <h2 className="text-center font-medium">Sua sacola está vazia.</h2>
-      )}
-    </div>
+                  <div className="flex items-center justify-between text-xs font-semibold">
+                    <span>Total</span>
+                    <span>{formatCurrency(totalPrice)}</span>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+
+            {/*Finalizar Pedidos */}
+            <Button
+              className="mt-6 w-full"
+              onClick={() => setIsConfirmDialogOpen(true)}
+              disabled={isSubmitLoading}
+            >
+              Finalizar Pedido
+            </Button>
+          </>
+        ) : (
+          <h2 className="text-center font-medium">Sua sacola está vazia.</h2>
+        )}
+      </div>
+
+      <AlertDialog
+        open={isConfirmDialogOpen}
+        onOpenChange={setIsConfirmDialogOpen}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Deseja Finalizar seu Pedido?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Ao finalizar seu pedido, você concorda com os termos e condições
+              da nossa plataforma.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction onClick={handleFinishOrderClick}>
+              {isSubmitLoading && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Finalizar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
         </div>
 
         <Sheet>
-          <SheetTrigger>
+          <SheetTrigger asChild>
             <Button
               size="icon"
               variant="outline"
@@ -50,96 +50,99 @@ const Header = () => {
             >
               <MenuIcon />
             </Button>
+          </SheetTrigger>
 
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle className="text-left">Menu</SheetTitle>
-              </SheetHeader>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="text-left">Menu</SheetTitle>
+            </SheetHeader>
 
-              {data?.user ? (
-                <>
-                  <div className="flex justify-between pt-6">
-                    <div className="flex items-center gap-3">
-                      <Avatar>
-                        <AvatarImage
-                          src={data?.user?.image as string | undefined}
-                        />
-                        <AvatarFallback>
-                          {data?.user?.name?.split(" ")[0][0]}
-                          {data?.user?.name?.split(" ")[1][0]}
-                        </AvatarFallback>
-                      </Avatar>
+            {data?.user ? (
+              <>
+                <div className="flex justify-between pt-6">
+                  <div className="flex items-center gap-3">
+                    <Avatar>
+                      <AvatarImage
+                        src={data?.user?.image as string | undefined}
+                      />
+                      <AvatarFallback>
+                        {data?.user?.name?.split(" ")[0][0]}
+                        {data?.user?.name?.split(" ")[1][0]}
+                      </AvatarFallback>
+                    </Avatar>
 
-                      <div>
-                        <h3 className="font-semibold">{data?.user?.name}</h3>
-                        <span className="block text-xs text-muted-foreground">
-                          {data?.user?.email}
-                        </span>
-                      </div>
+                    <div>
+                      <h3 className="font-semibold">{data?.user?.name}</h3>
+                      <span className="block text-xs text-muted-foreground">
+                        {data?.user?.email}
+                      </span>
                     </div>
                   </div>
-                </>
-              ) : (
-                <>
-                  <div className="flex items-center justify-between pt-10">
-                    <h2 className="font-semibold">Olá, Faça seu login!</h2>
-                    <Button size="icon" onClick={handleSignInClick}>
-                      <LogInIcon />
-                    </Button>
-                  </div>
-                </>
-              )}
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex items-center justify-between pt-10">
+                  <h2 className="font-semibold">Olá, Faça seu login!</h2>
+                  <Button size="icon" onClick={handleSignInClick}>
+                    <LogInIcon />
+                  </Button>
+                </div>
+              </>
+            )}
 
-              <div className="py-6">
-                <Separator />
-              </div>
+            <div className="py-6">
+              <Separator />
+            </div>
 
-              <div className="space-y-2">
-                <Button
-                  variant="ghost"
-                  className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
-                >
-                  <HomeIcon size={16} />
-                  <span className="block">Inicio</span>
-                </Button>
-
-                {data?.user && (
-                  <>
-                    <Button
-                      variant="ghost"
-                      className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
-                    >
-                      <ScrollTextIcon size={16} />
-                      <span className="block">Meus Pedidos</span>
-                    </Button>
-
-                    <Button
-                      variant="ghost"
-                      className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
-                    >
-                      <HeartIcon size={16} />
-                      <span className="block">Restaurantes Favoritos</span>
-                    </Button>
-                  </>
-                )}
-              </div>
-
-              <div className="py-6">
-                <Separator />
-              </div>
+            <div className="space-y-2">
+              <Button
+                variant="ghost"
+                className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
+              >
+                <HomeIcon size={16} />
+                <span className="block">Inicio</span>
+              </Button>
 
               {data?.user && (
-                <Button
-                  variant="ghost"
-                  className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
-                  onClick={handleSignOutClick}
-                >
-                  <LogOutIcon size={16} />
-                  <span className="block">Sair da Conta</span>
-                </Button>
+                <>
+                  <Button
+                    variant="ghost"
+                    className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
+                    asChild
+                  >
+                    <Link href="/my-orders">
+                      <ScrollTextIcon size={16} />
+                      <span className="block">Meus Pedidos</span>
+                    </Link>
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
+                  >
+                    <HeartIcon size={16} />
+                    <span className="block">Restaurantes Favoritos</span>
+                  </Button>
+                </>
               )}
-            </SheetContent>
-          </SheetTrigger>
+            </div>
+
+            <div className="py-6">
+              <Separator />
+            </div>
+
+            {data?.user && (
+              <Button
+                variant="ghost"
+                className="font-nornal w-full justify-start space-x-3 rounded-full text-xs"
+                onClick={handleSignOutClick}
+              >
+                <LogOutIcon size={16} />
+                <span className="block">Sair da Conta</span>
+              </Button>
+            )}
+          </SheetContent>
         </Sheet>
       </div>
     </>
